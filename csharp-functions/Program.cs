7da4aa7b6269c4360781af302f5ea9b1d1524f5b
leonardoserra <serra.leonardo92@gmail.com>
2023-09-07 15:35:55 +0200

    funzione 3 ok

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,11 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
+
+
+
+
 //Utility Functions
 
-/*      preso un array di numeri interi,
-        stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”.
-        Potete prendere quella fatta in classe questa mattina
-*/
+//preso un array di numeri interi,stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”.
+
 void StampaArray(int[] array)
 {
     Console.Write("[");
@@ -19,8 +23,22 @@ void StampaArray(int[] array)
     Console.Write("]");
 }
 
-
-// restituisca il quadrato del numero passato come parametro.
+//      restituisca il quadrato del numero passato come parametro.
 int Quadrato(int numero) {
     return numero * numero;
+}
+
+/*
+che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. 
+Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
+ */
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayCopy = (int[])array.Clone();
+    for(int i = 0; i < arrayCopy.Length; i++)
+    {
+        arrayCopy[i] = Quadrato(arrayCopy[i]);
+    }
+    return arrayCopy;
 }
